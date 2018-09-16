7c6398d0856a75e437e3f75e497235207bdb7070
Abdo <abdo@abdo.com>
2018-09-16 10:59:45 +0200

    add long click to channel tabs

--- BlogWebsite/wwwroot/JavaScript/BlogScript.js
+++ BlogWebsite/wwwroot/JavaScript/BlogScript.js
@@ -56,3 +56,32 @@ function iconHoverOut(element) {
     element.style.border ="2px solid transparent";
 }
 
+/*Long click event and context menu for channel folders and files*/
+
+//1-Long Click
+var timer;
+var isTrue;
+function onLongClick() {
+    var delay = 500;   //how much long to hold
+    isTrue = true;
+    timer = setTimeout(function () { changes() }, delay);
+}
+
+function changes() {
+   
+    if (timer) 
+        clearTimeout(timer);
+   
+    if (isTrue)
+        alert("Good start");
+    
+}
+
+function reset() {
+    isTrue = false;
+}
+
+
+
+
+
