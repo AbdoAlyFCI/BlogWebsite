b0d018571163715206ab9e96d00e5f4bb8ee93d1
Abdo <abdo@abdo.com>
2018-09-16 18:35:17 +0200

    add context menu

--- BlogWebsite/wwwroot/JavaScript/BlogScript.js
+++ BlogWebsite/wwwroot/JavaScript/BlogScript.js
@@ -1,4 +1,6 @@
-﻿/*Sign Up page*/
+﻿
+
+/*Sign Up page*/
 
 //List of Years
 (function YearList() {
@@ -45,41 +47,6 @@
     }
 })();
 
-/*ChannelPage*/
-function iconHoverOver(element) {
-    element.style.backgroundColor = 'rgba(8, 73, 244, 0.46)';
-    element.style.border = '2px solid blue';
-}
-
-function iconHoverOut(element) {
-    element.style.backgroundColor = "transparent";
-    element.style.border ="2px solid transparent";
-}
-
-/*Long click event and context menu for channel folders and files*/
-
-//1-Long Click
-var timer;
-var isTrue;
-function onLongClick() {
-    var delay = 500;   //how much long to hold
-    isTrue = true;
-    timer = setTimeout(function () { changes() }, delay);
-}
-
-function changes() {
-   
-    if (timer) 
-        clearTimeout(timer);
-   
-    if (isTrue)
-        alert("Good start");
-    
-}
-
-function reset() {
-    isTrue = false;
-}
 
 
 
---
+++ BlogWebsite/wwwroot/JavaScript/ChannelContent.js
@@ -0,0 +1,37 @@
+﻿/*ChannelPage*/
+function iconHoverOver(element) {
+    element.style.backgroundColor = 'rgba(8, 73, 244, 0.46)';
+    element.style.border = '2px solid blue';
+}
+
+function iconHoverOut(element) {
+    element.style.backgroundColor = "transparent";
+    element.style.border = "2px solid transparent";
+}
+
+/*Long click event and context menu for channel folders and files*/
+
+//1-Long Click
+var timer;
+var isTrue;
+function onLongClick() {
+    var delay = 500;   //how much long to hold
+    isTrue = true;
+    timer = setTimeout(function () { changes() }, delay);
+}
+
+function changes() {
+
+    if (timer)
+        clearTimeout(timer);
+
+    if (isTrue)
+        alert("Good start");
+
+}
+
+function reset() {
+    isTrue = false;
+}
+
+
