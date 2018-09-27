cc7af22be40c34d03ee69e5f42514c92e0fc2cf4
Abdo <abdo@abdo.com>
2018-09-27 17:54:07 +0200

    made change in feed page

---
+++ BlogWebsite/wwwroot/JavaScript/Feed.js
@@ -0,0 +1,15 @@
+﻿function iconOver(e) {
+    
+    
+    e.childNodes[1].hidden = true;
+    e.childNodes[3].hidden = false;
+    
+}
+
+function iconOut (e) {
+    e.childNodes[1].hidden = false;
+    e.childNodes[3].hidden = true;
+}
+
+//var e = document.getElementsByClassName("Save")[0];
+//console.log(e.childNodes);
